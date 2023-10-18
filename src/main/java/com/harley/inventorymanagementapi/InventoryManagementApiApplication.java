77cce8ac48e614aeb9297e44a460cb0087d2cf97
Harley <harley201009@hotmail.com>
2023-10-17 22:35:49 -0300

    Creating inventory management project

---
+++ src/main/java/com/harley/inventorymanagementapi/InventoryManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.harley.inventorymanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harley/inventorymanagementapi/InventoryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harley.inventorymanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
